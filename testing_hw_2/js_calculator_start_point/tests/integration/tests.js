891f505a3bc0323443434355a2c43053dabb7e89
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-06-10 10:06:31 +0100

    testing 2 complete

--- testing_hw_2/js_calculator_start_point/tests/integration/tests.js
+++ testing_hw_2/js_calculator_start_point/tests/integration/tests.js
@@ -11,20 +11,22 @@ describe('calculator functionality', function() {
 
   // write integration tests here in the form of "it should do something..."
 
-  it('should have working number buttons', function(){
+  it('should have buttons that update the display of the running total', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-  it('concatenate multiple button clicks', function(){
+  it('should update the display with the results of arithmetical operations', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
     element(by.css('#number2')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('22')
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
   })
 
-  it('chain multiple operations together', function(){
+  it('should be able to chain operations', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     element(by.css('#operator_multiply')).click();
@@ -35,7 +37,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('8')
   })
 
-  it('clear the running total without affecting the calculation', function(){
+  it('should clear the running total without affecting the calculation', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     element(by.css('#operator_multiply')).click();
@@ -45,5 +47,56 @@ describe('calculator functionality', function() {
     element(by.css('#clear')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('0')
   })
+
+  it('should be able to go negative', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-6')
+  })
+
+  it('should be able to display decimel', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4.5')
+  })
+
+  it('should be able to display large numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10000000000000')
+  })
+
+
+  it('should be display NaN if dividing by zero', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('NaN')
+  })
+
 
 });
